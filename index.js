065f649de7b574d34dad088cc81aececda665d2a
Ara Atzuri <hyalithus@gmail.com>
2016-08-16 21:48:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var cart;
+var cart = [];
 
 function setCart(newCart) {
   cart = newCart;
@@ -15,3 +15,29 @@ function total() {
 
   return t
 }
+
+function getCart() {
+  return cart
+} // getCart
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * (101 - 0));
+  cart.push({item: price});
+  console.log(`${item} has been added to your cart.`);
+  return cart
+} //
+
+function viewCart() {
+  if (cart.length == 0)
+    return `Your shopping cart is empty.`
+
+  else {
+    var string = `In your cart you have`
+
+    for (var item in cart) {
+      string += `${cart[item]} at $${item}`
+    }
+
+    return string
+  }
+} // viewCart
