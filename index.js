922fe6a7e6103ca8bb0c695c5b81ce7036c88f4d
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-06-20 23:00:10 +0000

    Done.

--- index.js
+++ index.js
@@ -15,25 +15,32 @@ function addToCart(item) {
   cart.push({[item] : price});
   console.log(`${item} has been added to your cart.`)
   return cart 
+  
 }
+
+// In order to get to the key/value pair you first need to iterate over the Object after iterating over each object then you iterate ovet each objects key:value.~Sidhe
+
 function viewCart() {
-  // write your code here
-  // if cart is empty 
-  if(cart.length == 0){
-   console.log("Your shopping cart is empty.")
-    return
-  }
- 
-  }
-  for(var i = 0; i < cart.length; i++){
-    var item = cart.item ;
-    var price = cart.price;
-    console.log(`In your cart, you have ${item} at ${price}.`);
-    // iterate over every item(object) in cart and print:
- // 1 item — In your cart, you have bananas at $17.2 items — In your cart, you have bananas at $17 and pancake batter at $5.3+ items — In your cart, you have bananas at $17, pancake batter at $5, and eggs at $49.
- 
-  }
- 
+   // console.log(`In your cart, you have ${item} at ${price}.`);
+    if (cart.length == 0) {
+      console.log("Your shopping cart is empty.");
+      return;
+    }
+    
+    for (var i in cart){
+    for (var item in cart[i])
+    	console.log(`In your cart, you have ${item} at $${cart[i][item]}.`);
+      if (cart.length > 1){
+    	  console.log(`In your cart, you have ${item} at $${cart[i][item]} and ${item} at $${cart[i][i][item]} .`);
+      }
+}
+  
+}
+
+
+
+
+
 // The total() function accepts no arguments, iterates through the cart array, and returns the current total value of the items in the cart.
 function total() {
   // write your code here
@@ -45,4 +52,9 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(cardNumber === undefined) {
+     console.log("We don't have a credit card on file for you to place your order.");
+  
+}
+cart.length = 0;
 }
