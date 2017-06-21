d1e3359ba8e33cf247b68b57e3b0407e5f532ad8
Yvonne Dreptate <yvonnedreptate@gmail.com>
2017-06-21 18:17:17 +0000

    Done.

--- index.js
+++ index.js
@@ -27,15 +27,21 @@ function viewCart() {
       return;
     }
     
+    var contentMessage = "In your cart, you have ";
     for (var i in cart){
     for (var item in cart[i])
-    	console.log(`In your cart, you have ${item} at $${cart[i][item]}.`);
-      if (cart.length > 1){
-    	  console.log(`In your cart, you have ${item} at $${cart[i][item]} and ${item} at $${cart[i][i][item]} .`);
-      }
+    if (i == 0)
+      contentMessage += `${item} at $${cart[i][item]}`;
+    else if (i > 0 && i < cart.length - 1)
+      contentMessage += `, ${item} at $${cart[i][item]}`;
+    else if (i == cart.length - 1 && cart.length == 2)
+      contentMessage += ` and ${item} at $${cart[i][item]}`;
+      else contentMessage += `, and ${item} at $${cart[i][item]}`;
+     }
+     
+     console.log(contentMessage + ".");
 }
   
-}
 
 
 
@@ -44,17 +50,29 @@ function viewCart() {
 // The total() function accepts no arguments, iterates through the cart array, and returns the current total value of the items in the cart.
 function total() {
   // write your code here
+  let totalPrice = 0
+  for (var i = 0; i < cart.length; i++){
+       for (var item in cart[i]){
+           totalPrice += cart[i][item]
+       }
+  }
+  return totalPrice
 }
 
 function removeFromCart(item) {
+     console.log("That item is not in your cart.")
+     return cart
   // write your code here
 }
 
 function placeOrder(cardNumber) {
   // write your code here
-  if(cardNumber === undefined) {
-     console.log("We don't have a credit card on file for you to place your order.");
+  if(!cardNumber) {
+     console.log('Sorry, we don\'t have a credit card on file for you.');
+     return false;
+  }
+     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`);
   
-}
+
 cart.length = 0;
 }
